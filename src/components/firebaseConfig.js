77bdb5d8232a09da12089ef1f8209a5b6e6817ea
mavibuz68 <65022902+mavibuz68@users.noreply.github.com>
2020-05-08 16:09:54 +0300

    first

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyDoP0vDgWtqzsWux7ioIS0ytsctanp7Gpc",
+    authDomain: "vuefs-prod-52b27.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-52b27.firebaseio.com",
+    projectId: "vuefs-prod-52b27",
+    storageBucket: "vuefs-prod-52b27.appspot.com",
+    messagingSenderId: "47839527226",
+    appId: "1:47839527226:web:fe900f35856e9187661003"
+}
